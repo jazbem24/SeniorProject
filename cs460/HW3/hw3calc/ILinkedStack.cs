c814f513f13f4e784a2a09dfb6094808548a767b
Jazmin Bembry <jbembry15@mail.wou.edu>
2017-10-26 22:19:30 -0700

    added interface for implementation of a linked stack

---
+++ cs460/HW3/hw3calc/ILinkedStack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace hw3calc
+{
+
+    /// <summary>
+    /// Linked stack Interface to be able to implement one for the calculator 
+    /// <typeparam name="T"> is used for the type (generic)
+    /// </summary>
+    public interface ILinkedStack<T>
+    {
+        ///
+
+        ///
+
+    }
+}
