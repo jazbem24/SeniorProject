a3a58231db93a234f0889caa23b58c942820611d
Jazmin Bembry <jbembry15@mail.wou.edu>
2017-11-05 20:23:40 -0800

    added bay area slang converter

--- cs460/HW4/hw4mvc/Controllers/HomeController.cs
+++ cs460/HW4/hw4mvc/Controllers/HomeController.cs
@@ -51,20 +51,89 @@ namespace hw4mvc.Controllers
             return View();
         }
 
-        // Get for page2
+        //GET ~/HOME/Page2
         [HttpGet]
         public ActionResult Page2()
         {
-            ViewBag.RequestMethod = "GET";
             return View();
-
         }
 
+        //POST ~/HOME/Page2
+        [HttpPost]
+        public ActionResult Page2(FormCollection form)
+        {
+            ViewBag.RequestMethod = "POST";
+            string firstSlang = Request.Form["firstDrop"];
+            string secondSlang = Request.Form["secondDrop"];
+
+            string compoundSlang;
+
+            //if-else statement for first drop down box
+            if (firstSlang == "ofCourse")
+            {
+                firstSlang = "fasho";
+            }
+            else if (firstSlang == "pickup")
+            {
+                firstSlang = "swoop";
+            }
+            else if (firstSlang == "dyu")
+            {
+                firstSlang = "yaddamean";
+            }
+            else if (firstSlang == "very")
+            {
+                firstSlang = "hella";
+            }
+            else if (firstSlang == "tryingTo")
+            {
+                firstSlang = "tryna";
+            }
+            else if (firstSlang == "yes")
+            {
+                firstSlang = "yee";
+            }
+
+            //if-else statement for second drop down box
+            if (secondSlang == "friend")
+            {
+                secondSlang = "cuddy";
+            }
+            else if (secondSlang == "cars")
+            {
+                secondSlang = "whips";
+            }
+            else if (secondSlang == "cool")
+            {
+                secondSlang = "wet";
+            }
+            else if (secondSlang == "leave")
+            {
+                secondSlang = "dip";
+            }
+            else if (secondSlang == "crazy")
+            {
+                secondSlang = "hyphy";
+            }
+            else if (secondSlang == "wrong")
+            {
+                secondSlang = "outta pocket";
+            }
+
+            compoundSlang = firstSlang + " " + secondSlang;
+            ViewBag.Message = compoundSlang;
+            return View();
+        }
+
+
         // GET ~/Home/Page3
+        [HttpGet]
         public ActionResult Page3()
         {
             return View();
         }
-
     }
+
 }
+
+
