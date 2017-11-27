f9b44a7ff20ca25d886dca9b3276fec1f29b254d
Jazmin.Bembry <jbembry15@mail.wou.edu>
2017-11-27 03:50:37 -0800

    added db_UP and db_DOWN scripts. Also got rid of some boilerpoint and added needed views and editted action links in the shared layout nav bar

---
+++ cs460/HW8/hw_8/hw_8/App_Data/db_DOWN.sql
@@ -0,0 +1,16 @@
+﻿--Alter to get rid of FK constraints, then drop them tables 
+
+ALTER TABLE[dbo].[ArtWork]
+DROP CONSTRAINT[FK_dbo.ArtWork_dbo.ArtistID];
+
+ALTER TABLE[dbo].[Classifications]
+DROP CONSTRAINT[FK_dbo.Classifications_dbo.ArtWorkID];
+	
+ALTER TABLE[dbo].[Classifications]
+DROP CONSTRAINT[FK_dbo.Classifications_dbo.GenreID];
+
+DROP TABLE Artists;
+DROP TABLE ArtWork;
+DROP TABLE Genre;
+DROP TABLE Classifications; 
+GO
---
+++ cs460/HW8/hw_8/hw_8/App_Data/db_UP.sql
@@ -0,0 +1,35 @@
+﻿--create Artist Table
+CREATE TABLE dbo.Artists(
+id int IDENTITY(1,1) NOT NULL,
+artistName varchar(128) NOT NULL,
+birthDate DateTime NOT NULL,
+birthPlace varchar(128) NOT NULL
+CONSTRAINT[PK.dbo.Artists] PRIMARY KEY CLUSTERED(ID ASC)
+);
+
+--create ArtWork table 
+CREATE TABLE dbo.ArtWork(
+id int IDENTITY(1,1) NOT NULL,
+title varchar(86) NOT NULL,
+ArtistID int NOT NULL, 
+CONSTRAINT[PK.dbo.ArtWork] PRIMARY KEY CLUSTERED(ID ASC),
+CONSTRAINT[FK_dbo.Artwork_dbo.ArtistID] FOREIGN KEY ([ArtistID])REFERENCES [dbo].[Artists]([ID])
+);
+
+--create Genre Table
+CREATE TABLE dbo.Genre(
+id int IDENTITY(1,1) NOT NULL,
+name varchar (64) NOT NULL
+CONSTRAINT[PK.dbo.Genre] PRIMARY KEY CLUSTERED(ID ASC)
+); 
+
+--create classifications Table 
+CREATE TABLE dbo.Classifications(
+id int IDENTITY(1,1) NOT NULL,
+ArtworkID int NOT NULL,
+GenreID int NOT NULL, 
+CONSTRAINT[PK.dbo.Classifications] PRIMARY KEY CLUSTERED(ID ASC),
+CONSTRAINT[FK_dbo.Classifications_dbo.ArtWorkID] FOREIGN KEY ([ArtworkID])REFERENCES [dbo].[ArtWork]([ID]),
+CONSTRAINT[FK_dbo.GenreID_dbo.GenreID] FOREIGN KEY ([GenreID])REFERENCES [dbo].[Genre]([ID])
+);
+GO
