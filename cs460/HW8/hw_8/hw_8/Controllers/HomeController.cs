e35fdb8bbd3aa157375cf447cddf5bcf15be0a0d
Jazmin.Bembry <jbembry15@mail.wou.edu>
2017-11-27 23:26:13 -0800

    artist crud pages generated

--- cs460/HW8/hw_8/hw_8/Controllers/HomeController.cs
+++ cs460/HW8/hw_8/hw_8/Controllers/HomeController.cs
@@ -1,30 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using hw_8.Models;
 
 namespace hw_8.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private ArtistryContext db = new ArtistryContext();
+
+        // GET: Home
+        public ActionResult Artists()
+        {
+            return View(db.Artists.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult ArtistsDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        // GET: Home/CreateArtists
+        public ActionResult CreateArtists()
         {
             return View();
         }
 
-        public ActionResult About()
+        // POST: Home/CreateArtists
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateArtists([Bind(Include = "ID,ArtistName,BirthDate,BirthCity")] Artist artist)
         {
-            ViewBag.Message = "Your application description page.";
+            if (ModelState.IsValid)
+            {
+                db.Artists.Add(artist);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-            return View();
+            return View(artist);
         }
 
-        public ActionResult Contact()
+        // GET: Home/EditArtist/5
+        public ActionResult EditArtist(int? id)
         {
-            ViewBag.Message = "Your contact page.";
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
 
-            return View();
+        // POST: Home/EditArtist/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditArtist([Bind(Include = "ID,ArtistName,BirthDate,BirthCity")] Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(artist).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(artist);
+        }
+
+        // GET: Home/DeleteArtist/5
+        public ActionResult DeleteArtist(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("DeleteArtist")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteArtistConfirmed(int id)
+        {
+            Artist artist = db.Artists.Find(id);
+            db.Artists.Remove(artist);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
